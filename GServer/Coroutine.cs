bcba890de3137516e9d98624c0d298b2151b8d59
ndhisrfzs <hissorrow@sina.com>
2015-10-30 18:11:59 +0800

    Coroutine 测试

---
+++ GServer/Coroutine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Threading;
+
+namespace GServer
+{
+    public class Coroutine
+    {
+        private IEnumerator mIterator;
+        public int session { get { return (int)mIterator.Current; } }
+
+        public Coroutine(IEnumerator iterator)
+        {
+            mIterator = iterator;
+            if (mIterator != null)
+            {
+                mIterator.MoveNext();
+            }
+        }
+
+        public void Resume(object result)
+        {
+            Thread.SetData(Thread.GetNamedDataSlot("slot"), result);
+            if (mIterator != null)
+            {
+                mIterator.MoveNext();
+            }
+        }
+    }
+}
---
+++ GServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace GServer
+{
+    class Program
+    {
+        static Dictionary<int, Coroutine> waits = new Dictionary<int, Coroutine>();
+        static void Main(string[] args)
+        {
+            Coroutine co = new Coroutine(test());
+            waits[co.session] = co;
+            while (true)
+            {
+                string cmds = Console.ReadLine();
+                string[] messages = cmds.Split(new string[]{" "}, StringSplitOptions.RemoveEmptyEntries);
+                int session = int.Parse(messages[0]);
+                Coroutine c = null;
+                if (waits.TryGetValue(session, out c))
+                {
+                    c.Resume(messages[1]);
+                }
+            }
+        }
+
+        static IEnumerator test()
+        {
+            int i = 1;
+            Console.WriteLine("step "+ i++);
+            yield return Call("call 1");
+            Console.WriteLine("Data:" + Thread.GetData(Thread.GetNamedDataSlot("slot")));           
+            Console.WriteLine("step " + i++);
+        }
+
+        static int session = 0;
+        static int Call(string message)
+        {
+            Console.WriteLine(message);
+            return Interlocked.Increment(ref session);
+        }
+    }
+}
